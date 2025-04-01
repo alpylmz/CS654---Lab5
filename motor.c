81ddca45b36668b68b7e0737e89195a43751a270
Alp Eren Yılmaz <41516584+alpylmz@users.noreply.github.com>
2025-04-01 18:20:07 -0400

    Update motor.c

--- motor.c
+++ motor.c
@@ -35,10 +35,11 @@ Set the duty cycle of the specified motor channel using
 the provided duty in microseconds. Use Timer2 for the duty cycle.
  */
 void motor_set_duty(uint8_t chan, uint16_t duty_us){
-    double ratio_us = duty_us/20000;
-    double duty_ratio = ratio_us * 4000;
-    double inverted = 4000 - duty_ratio;
-    
+    double ratio_us = ((double)duty_us)/20000.0;
+    double duty_ratio = ratio_us * 4000.0;
+    double inverted = 4000.0 - duty_ratio;
+    //double val = (12.8*1000000) / (64/duty_us);
+    //double inverted = PR2 - val;
     // safety check according to lab manual
     //if(duty_us <= 210000 && duty_us >= 90000){
         if(chan == 0){ // for X axis        
