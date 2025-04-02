5323b9815c036bfe9ff5675799805dd3eaae02b8
Alp Eren Yılmaz <41516584+alpylmz@users.noreply.github.com>
2025-04-01 20:21:29 -0400

    Update motor.c

--- motor.c
+++ motor.c
@@ -7,6 +7,10 @@
 
 
 #include "motor.h"
+#include <stdlib.h>
+int prev_x = 0;
+int prev_y = 0;
+
 
 void motor_init(uint8_t chan){
     /* Disable Timer 2 */
@@ -18,7 +22,7 @@ void motor_init(uint8_t chan){
     T2CONbits.TCKPS = 0b10;
     CLEARBIT(IEC0bits.T2IE);
     CLEARBIT(IFS0bits.T2IF);
-    PR2 = 8000;
+    PR2 = 4000;
     if(chan == 0){ // for X axis
         /*Configure Output Compare 8 (X-axis motor) */
         CLEARBIT(TRISDbits.TRISD7); // Set OC8 = RD7 as output
@@ -27,30 +31,47 @@ void motor_init(uint8_t chan){
     }
     /* Enable Timer 2 */
     SETBIT(T2CONbits.TON);
+    OC8CONbits.OCM = 0b110; // Set PWM, no fault mode
 }
 
 
+double mapValue(double x, double in_min, double in_max, double out_min, double out_max){
+    if(in_max == in_min){
+        return out_min;
+    }
+    return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+    
+}
 /*
 Set the duty cycle of the specified motor channel using 
 the provided duty in microseconds. Use Timer2 for the duty cycle.
  */
 void motor_set_duty(uint8_t chan, uint16_t duty_us){
-    duty_us = duty_us / 5;
     double ratio_us = ((double)duty_us)/20000.0;
     double duty_ratio = ratio_us * PR2;
-    double inverted = PR2 - duty_ratio;
+    //duty_ratio = mapValue(duty_ratio, 200, 400, 2000, 3000);
+    lcd_locate(1,6);
+    lcd_printf("%f", duty_ratio);
+    double inverted = (PR2 - duty_ratio);
     //double val = (12.8*1000000) / (64/duty_us);
     //double inverted = PR2 - val;
     // safety check according to lab manual
     //if(duty_us <= 210000 && duty_us >= 90000){
-        if(chan == 0){ // for X axis        
+        if(chan == 0){ // for X axis       
+            lcd_locate(1,7);
+            lcd_printf("%f", inverted);
+            if(abs(prev_x - inverted) < 10){
+                return;
+            }
             OC8R = inverted; // First duty cycle of x ms
             OC8RS = inverted; // Next duty cycle of x ms
-            OC8CONbits.OCM = 0b110; // Set PWM, no fault mode
+            //OC8CONbits.OCM = 0b110; // Set PWM, no fault mode
+            prev_x = inverted;
         }else if (chan == 1){
             OC7R = inverted; // First duty cycle of y ms
             OC7RS = inverted; // Next duty cycle of y ms
             OC7CONbits.OCM = 0b110; // Set PWM, no fault mode
+            prev_y = inverted;
         }
     //}
 }
